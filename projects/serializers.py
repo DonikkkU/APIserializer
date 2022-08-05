d017c124b6085d4191f38d267c4c19d6ac6d76e2
EdisonPTS <96064087+Jkamoliddin@users.noreply.github.com>
2022-08-05 17:06:51 +0500

    update

--- projects/serializers.py
+++ projects/serializers.py
@@ -11,3 +11,8 @@ class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
         fields = '__all__'
+
+class SkillSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Skill
+        fields = '__all__'
