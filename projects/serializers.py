eed79ce8c0d189f0eff1668a52abf3f74403be0d
Donik <umarovdoniyor096@gmail.com>
2022-08-05 14:05:43 +0500

    update

---
+++ projects/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import *
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ['id', 'title', 'description']
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
